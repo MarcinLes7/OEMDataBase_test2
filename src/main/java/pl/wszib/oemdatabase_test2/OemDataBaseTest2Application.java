ef2f0b87731aa614beda60898d93cafbee0d4683
MarcinLes7 <Gitm@rtino7>
2023-08-11 13:36:11 +0200

    Initial commit

---
+++ src/main/java/pl/wszib/oemdatabase_test2/OemDataBaseTest2Application.java
@@ -0,0 +1,13 @@
+package pl.wszib.oemdatabase_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemDataBaseTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OemDataBaseTest2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wszib/oemdatabase_test2/OemDataBaseTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wszib.oemdatabase_test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OemDataBaseTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
